521ac9416995b8fbabc90448ee3533b55b32c677
Ilija Taseski <itaseski@t.mk>
2022-10-27 12:10:10 +0200

    hard-coded in the view

--- maint/views.py
+++ maint/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Vehicle
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the maint index.")
+    vehicle_list = Vehicle.objects.order_by('-assembly_date')
+    output = ', '.join([vehicle.vin for vehicle in vehicle_list])
+    return HttpResponse(output)
 
 def detail(request, vehicle_id):
     return HttpResponse("You're looking at vehicle %s." % vehicle_id)
