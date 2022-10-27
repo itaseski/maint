dcbc7b92c1f2215697a1d7cb69aa7c217776dd3c
Ilija Taseski <itaseski@t.mk>
2022-10-27 12:58:45 +0200

    BASE_DIR templates added

--- maint/views.py
+++ maint/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Vehicle
 
 
 def index(request):
     vehicle_list = Vehicle.objects.order_by('-assembly_date')
-    output = ', '.join([vehicle.vin for vehicle in vehicle_list])
-    return HttpResponse(output)
+    template = loader.get_template('maint/index.html')
+    context = {
+        'vehicle_list': vehicle_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, vehicle_id):
     return HttpResponse("You're looking at vehicle %s." % vehicle_id)
