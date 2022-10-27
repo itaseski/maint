02c1866a47d49ea54cb518feaa8fe27ca3bfa027
Ilija Taseski <itaseski@t.mk>
2022-10-27 11:56:44 +0200

    view detail added

--- maint/urls.py
+++ maint/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /maint/
     path('', views.index, name='index'),
+    # ex: /maint/3/
+    path('<int:vehicle_id>/', views.detail, name='detail'),
+    
+
 ]
--- maint/views.py
+++ maint/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the maint index.")
+
+def detail(request, vehicle_id):
+    return HttpResponse("You're looking at vehicle %s." % vehicle_id)
+
