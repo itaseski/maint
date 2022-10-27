d7eefb33b083fb22f985fb6982d5b3110b5a0d32
Ilija Taseski <itaseski@t.mk>
2022-10-27 13:10:04 +0200

    shortcut render

--- maint/views.py
+++ maint/views.py
@@ -1,16 +1,15 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Vehicle
 
 
 def index(request):
     vehicle_list = Vehicle.objects.order_by('-assembly_date')
-    template = loader.get_template('maint/index.html')
     context = {
         'vehicle_list': vehicle_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'maint/index.html', context)
 
 def detail(request, vehicle_id):
     return HttpResponse("You're looking at vehicle %s." % vehicle_id)
